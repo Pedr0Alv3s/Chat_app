03ffddc7e89970916b2d36b647abc045958eb503
Pedro <pedroammedeiros@gmail.com>
2026-03-24 14:25:27 -0300

    Add files via upload

---
+++ ChatApp/Server/src/main/java/com/example/ChatApp/model/User.java
@@ -0,0 +1,20 @@
+package com.example.ChatApp.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @Column(nullable = false, updatable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 45)
+    private String email;
+
+    @Column(nullable = false, length = 64)
+    private String password;
+
+    @Column(name = "first_name", nullable = false, length = 20)
+    private String firstName;
+}
