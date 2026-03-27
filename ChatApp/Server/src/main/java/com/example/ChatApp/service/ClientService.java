fe30712f58214cc9febed29cfe06ef78cf022e3a
Pedro <pedroammedeiros@gmail.com>
2026-03-27 13:05:36 -0300

    Método de login
    Implementado Login e Registro

--- ChatApp/Server/src/main/java/com/example/ChatApp/service/ClientService.java
+++ ChatApp/Server/src/main/java/com/example/ChatApp/service/ClientService.java
@@ -6,7 +6,6 @@ import com.example.ChatApp.dto.RegisterRequestDTO;
 import com.example.ChatApp.model.Client;
 import com.example.ChatApp.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import java.util.Optional;
@@ -14,15 +13,18 @@ import java.util.Optional;
 @Service
 public class ClientService {
 
+    // Criação de objetos Client repository ( irá fazer o contato com o banco)
     @Autowired
     ClientRepository clientRepository;
+    // Criação do objeto PasswordEncoder , responsavel por fazer o hash das senhas;
+    @Autowired
     PasswordEncoder passwordEncoder;
 
     public ClientService(ClientRepository clientRepository){
         this.clientRepository = clientRepository;
     }
 
-
+    // metodo de login
     public ClientResponseDTO login(LoginRequestDTO dto) {
         Optional<Client> optionalClient = clientRepository.findByEmail(dto.getEmail());
         if(optionalClient.isEmpty()){
@@ -44,7 +46,7 @@ public class ClientService {
 
         return response;
     }
-
+    // metodo de registro
     public ClientResponseDTO register(RegisterRequestDTO dto) {
         //verificação se cadastro ja existe;
         Optional<Client> clientexists = clientRepository.findByEmail(dto.getEmail());
@@ -52,20 +54,21 @@ public class ClientService {
             throw new RuntimeException("Email já cadastrado");
         }
 
-        // criação de entiade cliente com dados inseridos;
+        // criação de entidade cliente com dados inseridos;
         Client client = new Client();
         client.setName(dto.getName());
         client.setEmail(dto.getEmail());
-        client.setPassword(dto.getPassword());
-
+        client.setPassword(passwordEncoder.encode(dto.getPassword()));
+        // Salva no banco o cliente novo;
         clientRepository.save(client);
 
-        //Retorna a resposta de entidade cliente criada
+        //Seta as variaveis da resposta que retorna;
         ClientResponseDTO response = new ClientResponseDTO();
         response.setId(client.getId());
         response.setName(client.getName());
         response.setEmail(client.getEmail());
 
+        //Retorna a resposta de entidade cliente criada
         return response;
     }
 }
