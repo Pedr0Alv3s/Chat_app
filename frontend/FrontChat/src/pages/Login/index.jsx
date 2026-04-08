dc44d31c71cd1cd7a4f3b47d963f2cb1336833cf
AngeloSalvatti <angelosalvatti1@gmail.com>
2026-04-07 22:06:05 -0300

    pagina login base html

--- frontend/FrontChat/src/pages/Login/index.jsx
+++ frontend/FrontChat/src/pages/Login/index.jsx
@@ -1,11 +1,87 @@
+import React from "react";
+import styles from "../Login/styles.module.css";
+import { useNavigate } from "react-router-dom";
 function Login() {
-    return (
-        <div>
-            <h1>Login</h1>
-            <input type="text" placeholder="Usuário" />
-            <input type="password" placeholder="Senha" />
-            <button>Login</button>
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [cadUsername, setCadUsername] = React.useState("");
+  const [cadPassword, setCadPassword] = React.useState("");
+  const [confPassword, setConfPassword] = React.useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    // console.log("Username:", username);
+    // console.log("Password:", password);
+    if (username && password) {
+      console.log("redirecionando para home");
+      navigate("/");
+    } else {
+      console.log("preencha todos os campos corretamente");
+    }
+  };
+
+  const handleCadastro = () => {
+    // console.log("Username:", cadUsername);
+    // console.log("Password:", cadPassword);
+    // console.log("Confirm Password:", confPassword);
+    if (cadUsername && cadPassword && confPassword) {
+      if (cadPassword !== confPassword) {
+        console.log("senhas não coinsidem");
+      } else {
+        console.log("redirecionando para home");
+        navigate("/");
+      }
+    } else {
+      console.log("preencha todos os campos corretamente");
+    }
+  };
+
+  return (
+    <>
+      <div className={styles.container}>
+        <h1>Login</h1>
+        <div className={styles.loginInputs}>
+          <input
+            placeholder="Usuário"
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            placeholder="Senha"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button onClick={() => handleLogin()}>Login</button>
         </div>
-    );
+      </div>
+
+      <div className={styles.container}>
+        <h1>Cadastro de Usuário</h1>
+        <div className={styles.loginInputs}>
+          <input
+            placeholder="Usuário"
+            type="text"
+            value={cadUsername}
+            onChange={(e) => setCadUsername(e.target.value)}
+          />
+          <input
+            placeholder="Senha"
+            type="password"
+            value={cadPassword}
+            onChange={(e) => setCadPassword(e.target.value)}
+          />
+          <input
+            placeholder="Confirmar Senha"
+            type="password"
+            value={confPassword}
+            onChange={(e) => setConfPassword(e.target.value)}
+          />
+          <button onClick={() => handleCadastro()}>Cadastrar</button>
+        </div>
+      </div>
+    </>
+  );
 }
 export default Login;
