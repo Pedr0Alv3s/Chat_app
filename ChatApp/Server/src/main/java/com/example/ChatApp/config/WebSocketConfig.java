d34aa5d22880370e2a50fba463a4d9d10c48ad8e
GabssMoraes <gab.msilva@hotmail.com>
2026-04-12 21:46:02 -0300

    Arrumei o chat

--- ChatApp/Server/src/main/java/com/example/ChatApp/config/WebSocketConfig.java
+++ ChatApp/Server/src/main/java/com/example/ChatApp/config/WebSocketConfig.java
@@ -21,9 +21,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .addInterceptors(interceptor).setAllowedOrigins("*");;
-                //.withSockJS();    // endpoint de conexão opcional
-                //.setAllowedOrigins("*"); // ajuste em produção
+                .setAllowedOriginPatterns("*")
+                .addInterceptors(interceptor)
+                .withSockJS();
     }
 
     @Override
